361c723a8218943820f931ca99d9665744a29cdd
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2022-11-25 00:58:21 +0600

    feat: add house create functionality

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('houses.index')" :active="request()->routeIs('houses.index')">
+                        {{ __('Your Houses') }}
+                    </x-nav-link>
                 </div>
             </div>
 
