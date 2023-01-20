31eb923255f452c674211b9cc35ebfe6776bbf26
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-01-20 18:01:39 +0600

    fix: formatting

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,9 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     public $timestamps = false;
+
     use HasFactory;
 
     protected $fillable = [
-        'name', 'country'
+        'name', 'country',
     ];
 }
