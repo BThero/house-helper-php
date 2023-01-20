afff83228071a72189643ab676ce8a7c08b42c42
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-01-20 15:55:48 +0600

    fix: formatting

--- app/Services/CitiesAPI.php
+++ app/Services/CitiesAPI.php
@@ -4,11 +4,14 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class CitiesAPI {
+class CitiesAPI
+{
     private $api_url = 'https://countriesnow.space/api/v0.1/';
 
-    public function AllCountriesAndCities() {
+    public function AllCountriesAndCities()
+    {
         $response = Http::get($this->api_url.'countries');
-        return json_decode($response -> body());
+
+        return json_decode($response->body());
     }
 }
