624744f91445f4c703ba68b1cd225ed78558f1df
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-01-20 15:55:20 +0600

    feat: create cities API client & cities table

---
+++ app/Services/CitiesAPI.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class CitiesAPI {
+    private $api_url = 'https://countriesnow.space/api/v0.1/';
+
+    public function AllCountriesAndCities() {
+        $response = Http::get($this->api_url.'countries');
+        return json_decode($response -> body());
+    }
+}
