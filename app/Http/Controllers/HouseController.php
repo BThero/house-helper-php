cb9e39642f23c14a3d5825b3df277c63572c4033
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-01-20 15:20:38 +0600

    fix: formatting

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\HouseStoreRequest;
 use App\Models\House;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
