75e3b2c21b3a42c0bce406880c930d083b8832a3
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-01-20 17:01:21 +0600

    feat: add more functions to Cities API

--- app/Services/CitiesAPI.php
+++ app/Services/CitiesAPI.php
@@ -8,9 +8,25 @@ class CitiesAPI
 {
     private $api_url = 'https://countriesnow.space/api/v0.1/';
 
-    public function AllCountriesAndCities()
+    public function GetAllCountriesAndCities()
     {
         $response = Http::get($this->api_url.'countries');
+
+        return json_decode($response->body());
+    }
+
+    public function GetAllCitiesOfCountry(string $country)
+    {
+        $response = Http::post($this->api_url.'countries/cities', [
+            'country' => $country,
+        ]);
+
+        return json_decode($response->body());
+    }
+
+    public function GetAllCountries()
+    {
+        $response = Http::get($this->api_url.'countries/iso');
 
         return json_decode($response->body());
     }
